f135e409ff2988ebc0b0bc71ec5520dd8fdeb00c
anam-waheed <75274250+anam-waheed@users.noreply.github.com>
2021-04-07 19:25:48 -0700

    Add files via upload

--- sql_aw.js
+++ sql_aw.js
@@ -1,8 +1,29 @@
-function changeColor() {
-  if(document.getElementById("firstButton").style.backgroundColor == "white"){
-	  document.getElementById("firstButton").style.backgroundColor = "DeepSkyBlue";
+function changeColor() { document.getElementById("firstButton").style.backgroundColor == "white" ? document.getElementById("firstButton").style.backgroundColor = "DeepSkyBlue" : document.getElementById("firstButton").style.backgroundColor = "white"; }
+
+/*
+function changeColorSelect() {
+  if(document.getElementById("select").style.backgroundColor == "white"){
+	  document.getElementById("select").style.backgroundColor = "DeepSkyBlue";
   }
   else {
-	  document.getElementById("firstButton").style.backgroundColor = "white"
+	  document.getElementById("select").style.backgroundColor = "white"
   }
-}
+}*/
+function changeColorselect() { document.getElementById("select").style.backgroundColor == "white" ? document.getElementById("select").style.backgroundColor = "DeepSkyBlue" : document.getElementById("select").style.backgroundColor = "white"; }
+function changeColorupdate() { document.getElementById("update").style.backgroundColor == "white" ? document.getElementById("update").style.backgroundColor = "DeepSkyBlue" : document.getElementById("update").style.backgroundColor = "white"; }
+function changeColordelete() { document.getElementById("delete").style.backgroundColor == "white" ? document.getElementById("delete").style.backgroundColor = "DeepSkyBlue" : document.getElementById("delete").style.backgroundColor = "white"; }
+function changeColorinsertinto() { document.getElementById("insert into").style.backgroundColor == "white" ? document.getElementById("insert into").style.backgroundColor = "DeepSkyBlue" : document.getElementById("insert into").style.backgroundColor = "white"; }
+function changeColorcreatedatabase() { document.getElementById("create database").style.backgroundColor == "white" ? document.getElementById("create database").style.backgroundColor = "DeepSkyBlue" : document.getElementById("create database").style.backgroundColor = "white"; }
+function changeColoralterdatabase() { document.getElementById("alter database").style.backgroundColor == "white" ? document.getElementById("alter database").style.backgroundColor = "DeepSkyBlue" : document.getElementById("alter database").style.backgroundColor = "white"; }
+function changeColorcreatetable() { document.getElementById("create table").style.backgroundColor == "white" ? document.getElementById("create table").style.backgroundColor = "DeepSkyBlue" : document.getElementById("create table").style.backgroundColor = "white"; }
+function changeColoraltertable() { document.getElementById("alter table").style.backgroundColor == "white" ? document.getElementById("alter table").style.backgroundColor = "DeepSkyBlue" : document.getElementById("alter table").style.backgroundColor = "white"; }
+function changeColordroptable() { document.getElementById("drop table").style.backgroundColor == "white" ? document.getElementById("drop table").style.backgroundColor = "DeepSkyBlue" : document.getElementById("drop table").style.backgroundColor = "white"; }
+function changeColorcreateindex() { document.getElementById("create index").style.backgroundColor == "white" ? document.getElementById("create index").style.backgroundColor = "DeepSkyBlue" : document.getElementById("create index").style.backgroundColor = "white"; }
+function changeColordropindex() { document.getElementById("drop index").style.backgroundColor == "white" ? document.getElementById("drop index").style.backgroundColor = "DeepSkyBlue" : document.getElementById("drop index").style.backgroundColor = "white"; }
+function changeColorfrom() { document.getElementById("from").style.backgroundColor == "white" ? document.getElementById("from").style.backgroundColor = "DeepSkyBlue" : document.getElementById("from").style.backgroundColor = "white"; }
+function changeColorselectdistinct() { document.getElementById("select distinct").style.backgroundColor == "white" ? document.getElementById("select distinct").style.backgroundColor = "DeepSkyBlue" : document.getElementById("select distinct").style.backgroundColor = "white"; }
+function changeColorwhere() { document.getElementById("where").style.backgroundColor == "white" ? document.getElementById("where").style.backgroundColor = "DeepSkyBlue" : document.getElementById("where").style.backgroundColor = "white"; }
+function changeColorandorandnot() { document.getElementById("and, or and not").style.backgroundColor == "white" ? document.getElementById("and, or and not").style.backgroundColor = "DeepSkyBlue" : document.getElementById("and, or and not").style.backgroundColor = "white"; }
+function changeColororderby() { document.getElementById("order by").style.backgroundColor == "white" ? document.getElementById("order by").style.backgroundColor = "DeepSkyBlue" : document.getElementById("order by").style.backgroundColor = "white"; }
+function changeColordesc() { document.getElementById("desc").style.backgroundColor == "white" ? document.getElementById("desc").style.backgroundColor = "DeepSkyBlue" : document.getElementById("desc").style.backgroundColor = "white"; }
+function changeColorasc() { document.getElementById("asc").style.backgroundColor == "white" ? document.getElementById("asc").style.backgroundColor = "DeepSkyBlue" : document.getElementById("asc").style.backgroundColor = "white"; }
