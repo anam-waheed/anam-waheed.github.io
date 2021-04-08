89bf70d88d7c4d4d8e563ed41e84848a5d9d951d
anam-waheed <75274250+anam-waheed@users.noreply.github.com>
2021-04-07 17:57:22 -0700

    Add files via upload

---
+++ sql_aw.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  if(document.getElementById("firstButton").style.backgroundColor == "white"){
+	  document.getElementById("firstButton").style.backgroundColor = "DeepSkyBlue";
+  }
+  else {
+	  document.getElementById("firstButton").style.backgroundColor = "white"
+  }
+}
